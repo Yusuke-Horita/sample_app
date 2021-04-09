162e563b9d852ec185568f5e8d16cba6a260b194
Yusuke-Horita <somagawa645@gmail.com>
2021-04-09 22:56:34 +0900

    third commit

--- app/lib/rakuten.rb
+++ app/lib/rakuten.rb
@@ -6,43 +6,8 @@ class Rakuten
 	PASSWORD = ENV["PASSWORD"]
 	def self.get_coupon
 		starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-		#Chrome用のドライバ
 		driver = Selenium::WebDriver.for :chrome
 		wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-
-		# driver.get "https://order.my.rakuten.co.jp/?l-id=pc_header_func_ph"
-		# driver.find_element(:name, "u").send_keys USER_NAME
-		# driver.find_element(:name, "p").send_keys PASSWORD
-		# driver.find_element(:name, "submit").click
-
-
-		# CSV.open('all_comments.csv', 'w') do |csv|
-		# 	csv << ["注文日", "ショップ名", "合計金額", "配達方法"]
-		# 	3.times do
-		# 		25.times do |num|
-		# 			elements = driver.find_elements(:css, ".oDrDetailList .detail .listPageIcon")
-		# 			elements[num].click
-			
-		# 			delivery = driver.find_element(:css, ".shippingType").text
-
-		# 			if /メール便|日本郵便/ =~ driver.find_element(css:"body").text
-		# 				puts shop_name = driver.find_element(:css, ".shopName").text
-		# 				order_date = driver.find_element(:css, ".orderDate").text
-		# 				net_tot = driver.find_element(:css, ".netTot").text
-		# 				csv << [order_date, shop_name, net_tot, delivery]
-						
-		# 				driver.navigate.back
-		# 			else
-		# 				ap driver.find_element(:css, ".shopName").text
-		# 				driver.navigate.back
-		# 			end
-		# 		end
-		# 		driver.find_element(:link_text, "次の25件>>").click
-		# 	end
-		# end
-
-
-
 
 		driver.get "https://wantedly.com/signin_or_signup"
 		email_form = driver.find_element(:css, "#email")
@@ -51,115 +16,261 @@ class Rakuten
 		wait.until { driver.find_element(:css, "#password").displayed? }
 		driver.find_element(:css, "#password").send_keys PASSWORD
 		driver.find_element(:css, "#password").submit
-
 		wait.until { driver.find_element(:css, ".OptionalSelectorAbstract--leader").displayed? }
-		# driver.find_element(:css, ".OptionalSelectorAbstract--leader").click
-		# driver.find_element(:link_text, "東京").click
-
 		driver.get "https://www.wantedly.com/projects?type=mixed&page=1&occupation_types%5B%5D=jp__engineering&hiring_types%5B%5D=mid_career&fields%5B%5D=jp__mobile_engineer&fields%5B%5D=jp__web_engineer&fields%5B%5D=jp__systems_engineer&fields%5B%5D=jp__data_scientist&locations%5B%5D=tokyo"
-
 		# ------ driver.find_element(:css, "#comment____________").submit --------
 
 		inclusion_companys = []
-		exclusion_companys = []
+		exclusion_companys_1 = []
+		exclusion_companys_2 = []
+		exclusion_companys_3 = []
 
 		# begin
-			10.times do
+			100.times do |page|
 				10.times do |num|
-					driver.find_elements(:css, ".project-index-single-inner")[num].click
-					# js-descriptions 
-					driver.navigate.refresh
-					if /SES|常駐|プロジェクト先|クライアント先|客先/ =~ driver.find_element(:css, ".js-descriptions").text || /フロントエンド|CTO|デザイナー/ =~ driver.find_element(:css, ".project-tag.normal").text
-						hash = {}
-						hash[:category] = driver.find_element(:css, ".project-tag.large.normal").text
-						hash[:title] = driver.find_element(:css, ".project-title").text
-						hash[:company] = driver.find_element(:css, ".company-link").text
-						hash[:url] = driver.current_url
-						exclusion_companys << hash
-						ap hash[:title]
-						driver.navigate.back
-						# elements = driver.find_elements(:css, ".oDrDetailList .detail .listPageIcon")
-						# elements[num].click
-				
-						# delivery = driver.find_element(:css, ".shippingType").text
-
-						# if /メール便|日本郵便/ =~ driver.find_element(css:"body").text
-						# 	puts shop_name = driver.find_element(:css, ".shopName").text
-						# 	order_date = driver.find_element(:css, ".orderDate").text
-						# 	net_tot = driver.find_element(:css, ".netTot").text
-						# 	csv << [order_date, shop_name, net_tot, delivery]
-							
-						# 	driver.navigate.back
-						# else
-						# 	ap driver.find_element(:css, ".shopName").text
-						# 	driver.navigate.back
-						# end
-						# driver.find_element(:link_text, "次の25件>>").click
-					else
-						hash = {}
-						hash[:category] = driver.find_element(:css, ".project-tag.large.normal").text
-						hash[:title] = driver.find_element(:css, ".project-title").text
-						hash[:company] = driver.find_element(:css, ".company-link").text
-						hash[:url] = driver.current_url
-						inclusion_companys << hash
-						puts hash[:title]
-						driver.navigate.back
+					begin
+						driver.find_elements(:css, ".project-index-single-inner")[num]
+					rescue
+						next
 					end
+					5.times do |i|
+						driver.find_elements(:css, ".project-index-single-inner")[num].click
+						# if driver.current_url == "https://www.wantedly.com/projects?type=mixed&page=#{ page + 1 }&occupation_types%5B%5D=jp__engineering&hiring_types%5B%5D=mid_career&fields%5B%5D=jp__mobile_engineer&fields%5B%5D=jp__web_engineer&fields%5B%5D=jp__systems_engineer&fields%5B%5D=jp__data_scientist&locations%5B%5D=tokyo"
+						if driver.current_url.include?("https://www.wantedly.com/projects/") && driver.current_url.exclude?("https://www.wantedly.com/projects/mixed?")
+							5.times do
+								begin
+									driver.find_element(:css, ".js-descriptions")
+									break
+								rescue
+									driver.navigate.refresh
+									puts "・"
+								end
+							end
+							break
+						end
+						puts "!!!!!!!!!!!"
+						driver.navigate.refresh
+						wait.until { driver.find_element(:css, ".project-index-single-inner").displayed? }
+					end
+					begin
+						text = driver.find_element(:css, ".js-descriptions").text
+						if /プロジェクト先|クライアント先/ =~ text || /フロントエンド|フロントエンジニア|CTO|デザイナー/ =~ driver.find_element(:css, ".project-tag.normal").text
+							hash = {}
+							hash[:category] = driver.find_element(:css, ".project-tag.large.normal").text
+							hash[:company] = driver.find_element(:css, ".company-link").text
+							hash[:title] = driver.find_element(:css, ".project-title").text
+							hash[:url] = driver.current_url
+							exclusion_companys_1 << hash
+							ap hash[:title]
+						elsif /常駐|客先/ =~ text
+							hash = {}
+							hash[:category] = driver.find_element(:css, ".project-tag.large.normal").text
+							hash[:company] = driver.find_element(:css, ".company-link").text
+							hash[:title] = driver.find_element(:css, ".project-title").text
+							hash[:url] = driver.current_url
+							exclusion_companys_2 << hash
+							ap hash[:title]
+						elsif /SES/ =~ text
+							hash = {}
+							hash[:category] = driver.find_element(:css, ".project-tag.large.normal").text
+							hash[:company] = driver.find_element(:css, ".company-link").text
+							hash[:title] = driver.find_element(:css, ".project-title").text
+							hash[:url] = driver.current_url
+							exclusion_companys_3 << hash
+							ap hash[:title]
+						else
+							hash = {}
+							hash[:category] = driver.find_element(:css, ".project-tag.large.normal").text
+							hash[:company] = driver.find_element(:css, ".company-link").text
+							hash[:title] = driver.find_element(:css, ".project-title").text
+							hash[:url] = driver.current_url
+							inclusion_companys << hash
+							puts hash[:title]
+						end
+					rescue
+						text = driver.find_element(:css, ".js-descriptions").text
+						if /プロジェクト先|クライアント先/ =~ text || /フロントエンド|フロントエンジニア|CTO|デザイナー/ =~ driver.find_element(:css, ".job-type-tag").text
+							hash = {}
+							hash[:category] = driver.find_element(:css, ".job-type-tag").text
+							hash[:company] = driver.find_element(:css, ".company-name").text
+							hash[:title] = driver.find_element(:css, ".project-title").text
+							hash[:url] = driver.current_url
+							exclusion_companys_1 << hash
+							ap hash[:title]
+						elsif /常駐|客先/ =~ text || /フロントエンド|フロントエンジニア|CTO|デザイナー/ =~ driver.find_element(:css, ".job-type-tag").text
+							hash = {}
+							hash[:category] = driver.find_element(:css, ".job-type-tag").text
+							hash[:company] = driver.find_element(:css, ".company-name").text
+							hash[:title] = driver.find_element(:css, ".project-title").text
+							hash[:url] = driver.current_url
+							exclusion_companys_2 << hash
+							ap hash[:title]
+						elsif /SES/ =~ text || /フロントエンド|フロントエンジニア|CTO|デザイナー/ =~ driver.find_element(:css, ".job-type-tag").text
+							hash = {}
+							hash[:category] = driver.find_element(:css, ".job-type-tag").text
+							hash[:company] = driver.find_element(:css, ".company-name").text
+							hash[:title] = driver.find_element(:css, ".project-title").text
+							hash[:url] = driver.current_url
+							exclusion_companys_3 << hash
+							ap hash[:title]
+						else
+							hash = {}
+							hash[:category] = driver.find_element(:css, ".job-type-tag").text
+							hash[:company] = driver.find_element(:css, ".company-name").text
+							hash[:title] = driver.find_element(:css, ".project-title").text
+							hash[:url] = driver.current_url
+							inclusion_companys << hash
+							puts hash[:title]
+						end
+					end
+					driver.navigate.back
 				end
 				driver.find_element(:link_text, "次へ").click
-				wait.until { driver.find_element(:css, ".project-index-single-inner").displayed? }
-				puts "---"
+				puts "--- #{ page + 1 } ---"
+				5.times do
+					begin
+						wait.until { driver.find_element(:css, ".project-index-single-inner").displayed? }
+						break
+					rescue
+						driver.navigate.refresh
+					end
+				end
 			end
 
+			# CSV.open('inclusion_company.csv', 'w') do |csv|
+			# 	csv << ["", "ジャンル", "タイトル", "会社名", "URL"]
+			# 	inclusion_companys.each_with_index do |company, i|
+			# 		csv << [i + 1, company[:category], company[:title], company[:company], company[:url]]
+			# 	end
+			# end
+			# CSV.open('exclusion_company.csv', 'w') do |csv|
+			# 	csv << ["", "ジャンル", "タイトル", "会社名", "URL"]
+			# 	exclusion_companys.each_with_index do |company, i|
+			# 		csv << [i + 1, company[:category], company[:title], company[:company], company[:url]]
+			# 	end
+			# end
+
+			puts inclusion_companys.size
+			puts exclusion_companys_1.size
+			puts exclusion_companys_2.size
+			puts exclusion_companys_3.size
+
+			puts exclusion_companys_1
+
+			inclusion_companys.uniq!
+			exclusion_companys_1.uniq!
+			exclusion_companys_2.uniq!
+			exclusion_companys_3.uniq!
+
+			ap inclusion_companys.size
+			ap exclusion_companys_1.size
+			ap exclusion_companys_2.size
+			ap exclusion_companys_3.size
+
+			puts exclusion_companys_1
+
+			f_i = []
+			f_e_1 = []
+			f_e_2 = []
+			f_e_3 = []
+	
+			inclusion_companys.each do |company|
+				if index = f_i.rindex(f_i.select{|i| i[:company] == company[:company]}.last)
+					company[:company] = "└ " + company[:company]
+					f_i.insert(index + 1, company)
+				else
+					f_i << company
+				end
+			end
+			exclusion_companys_1.each do |company|
+				if index = f_e_1.rindex(f_e_1.select{|i| i[:company] == company[:company]}.last)
+					company[:company] = "└ " + company[:company]
+					f_e_1.insert(index + 1, company)
+				else
+					f_e_1 << company
+				end
+			end
+			exclusion_companys_2.each do |company|
+				if index = f_e_2.rindex(f_e_2.select{|i| i[:company] == company[:company]}.last)
+					company[:company] = "└ " + company[:company]
+					f_e_2.insert(index + 1, company)
+				else
+					f_e_2 << company
+				end
+			end
+			exclusion_companys_3.each do |company|
+				if index = f_e_3.rindex(f_e_3.select{|i| i[:company] == company[:company]}.last)
+					company[:company] = "└ " + company[:company]
+					f_e_3.insert(index + 1, company)
+				else
+					f_e_3 << company
+				end
+			end
+
+			puts f_i.size
+			puts f_e_1.size
+			puts f_e_2.size
+			puts f_e_3.size
+
+			puts f_e_1
+	
 			CSV.open('inclusion_company.csv', 'w') do |csv|
 				csv << ["", "ジャンル", "タイトル", "会社名", "URL"]
-				inclusion_companys.each_with_index do |company, i|
-					csv << [i + 1, company[:category], company[:title], company[:company], company[:url]]
+				f_i.each_with_index do |company, i|
+					csv << [i + 1, company[:category], company[:company], company[:title], company[:url]]
 				end
 			end
-
-			CSV.open('exclusion_company.csv', 'w') do |csv|
+			CSV.open('exclusion_company_1.csv', 'w') do |csv|
 				csv << ["", "ジャンル", "タイトル", "会社名", "URL"]
-				exclusion_companys.each_with_index do |company, i|
-					csv << [i + 1, company[:category], company[:title], company[:company], company[:url]]
+				f_e_1.each_with_index do |company, i|
+					csv << [i + 1, company[:category], company[:company], company[:title], company[:url]]
+				end
+			end
+			CSV.open('exclusion_company_2.csv', 'w') do |csv|
+				csv << ["", "ジャンル", "タイトル", "会社名", "URL"]
+				f_e_2.each_with_index do |company, i|
+					csv << [i + 1, company[:category], company[:company], company[:title], company[:url]]
+				end
+			end
+			CSV.open('exclusion_company_3.csv', 'w') do |csv|
+				csv << ["", "ジャンル", "タイトル", "会社名", "URL"]
+				f_e_3.each_with_index do |company, i|
+					csv << [i + 1, company[:category], company[:company], company[:title], company[:url]]
 				end
 			end
 
-			ap inclusion_companys.size + exclusion_companys.size
+			# ap inclusion_companys.size + exclusion_companys.size
 			ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 			elapsed = ending - starting
+			
 		# rescue
+		# 	CSV.open('inclusion_company.csv', 'w') do |csv|
+		# 		csv << ["", "ジャンル", "タイトル", "会社名", "URL"]
+		# 		inclusion_companys.each_with_index do |company, i|
+		# 			csv << [i + 1, company[:category], company[:title], company[:company], company[:url]]
+		# 		end
+		# 	end
+		# 	CSV.open('exclusion_company.csv', 'w') do |csv|
+		# 		csv << ["", "ジャンル", "タイトル", "会社名", "URL"]
+		# 		exclusion_companys.each_with_index do |company, i|
+		# 			csv << [i + 1, company[:category], company[:title], company[:company], company[:url]]
+		# 		end
+		# 	end
+
 		# 	ap inclusion_companys.size + exclusion_companys.size
 		# 	ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 		# 	elapsed = ending - starting
 		# end
 
-		# begin
-		# 	driver.find_element(:xpath, "//p[contains(text(), 'SES')]")
-		# 	puts driver.find_element(:css, ".company-logo")
-		# 	driver.navigate.back
-		# rescue
-		# 	begin
-		# 		driver.find_element(:xpath, "//p[contains(text(), '常駐')]")
-		# 		puts driver.find_element(:css, ".company-logo")
-		# 		driver.navigate.back
-		# 	rescue
-		# 		begin
-		# 			driver.find_element(:xpath, "//p[contains(text(), 'ses')]")
-		# 			puts driver.find_element(:css, ".company-logo")
-		# 			driver.navigate.back
-		# 		rescue
-		# 			begin
-		# 				driver.find_element(:xpath, "//div[contains(text(), 'SES')]")
-		# 				puts driver.find_element(:css, ".company-logo")
-		# 				driver.navigate.back
-		# 			rescue
-		# 				driver.navigate.back
-		# 			end
-		# 		end
-		# 	end
-		# end
+
+		# driver.find_element(:xpath, "//p[contains(text(), 'SES')]")
 
 	end
-end
+	# a.find{|i| i[:f] == "F"}
+	# a.rindex({:e=>"E", :f=>"F"})
+	# =>
+	# a.rindex(a.find{|i| i[:f] == "F"})
 
+	# a.insert(index + 1, ccc)
+end
+"https://www.wantedly.com/projects/mixed?fields%5B%5D=jp__mobile_engineer&fields%5B%5D=jp__web_engineer&fields%5B%5D=jp__systems_engineer&fields%5B%5D=jp__data_scientist&hiring_types%5B%5D=mid_career&locations%5B%5D=tokyo&occupation_types%5B%5D=jp__engineering&page=42"
